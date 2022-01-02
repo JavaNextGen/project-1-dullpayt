b53abc6d74957eb8cdcd5f1833ee96a108c06046
Payton Dull <payton.dull@revature.net>
2022-01-01 21:49:35 -0500

    established connection to SQL

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,7 +1,45 @@
 package com.revature;
 
+import java.sql.Connection; 
+import java.sql.SQLException;
+
+import com.revature.models.Employee;
+import com.revature.models.FinanceManager;
+import com.revature.util.ConnectionFactory;
+import com.revature.models.Reimbursement;
+
+
+
 public class Driver {
 
     public static void main(String[] args) {
-    }
+    	
+    	//Testing Database Connectivity - just testing whether our Connection (from ConnectionFactory) is successful
+		try(Connection conn = ConnectionFactory.getConnection()){
+			System.out.println("Connection Successful :)");
+		} catch(SQLException e) {
+			System.out.println("Connection failed");
+			e.printStackTrace();
+		}
+		
+		//Here is the actual functionality of our application-------------------
+		
+		//spoiler alert... there will only be two lines of codes here
+		//
+		
+		//Instantiate a menu object
+		Employee menu = new Employee();
+		
+		//Use the Menu Class's displayMenu() method to give the user the menu
+		menu.displayMenu();
+		
+		
+		//this is our entire main method (until we learn Javalin in week 4)
+		
+		//all of the complicated menu logic is hidden in the Menu class... this is power of abstraction!!!!!
+		
+	}
+    	
 }
+    
+
